448500fd1eb27b6139fe809657e62f353a62c2a0
Shant Derounian <shantd9@hotmail.com>
2020-01-14 09:07:13 +0200

    added reactive forms and authservice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { BlogListComponent } from './components/blog-list/blog-list.component';
-
+import { AuthGuard } from './helpers/auth.guard';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '', pathMatch: 'full', redirectTo: 'login',  canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'blogs', component: BlogListComponent },
